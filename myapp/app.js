baa0f45155c2d4a2bdb1aa46ce79c31c380532d8
winsonngoh <35997618+winsonngoh@users.noreply.github.com>
2019-04-17 20:44:28 +0200

    Error handler

--- myapp/app.js
+++ myapp/app.js
@@ -16,4 +16,18 @@ app.post('/', function (req, res) {
     res.send('Got a DELETE request at /user')
   })
 
+  //Handle 404 errors
+  app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
+  })
+
+
+
+  //Voorbeeld error handler
+  //You define error-handling middleware in the same way as other middleware, 
+  //except with four arguments instead of three; specifically with the signature (err, req, res, next):
+  app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
